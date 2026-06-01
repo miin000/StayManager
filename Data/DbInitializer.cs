e12e635d2feca3070d17bfb11fd08b7bcbb9ce6c
miin0 <phammin000@gmail.com>
2026-06-01 10:02:12 +0700

    init project

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.Entity;
+
+namespace QuanLyKho.Data
+{
+    public class DbInitializer : IDatabaseInitializer<AppDbContext>
+    {
+        public void InitializeDatabase(AppDbContext context)
+        {
+            context.Database.ExecuteSqlCommand(@"
+                CREATE TABLE IF NOT EXISTS Categories (
+                    CategoryId INTEGER PRIMARY KEY AUTOINCREMENT,
+                    TenDanhMuc NVARCHAR(100) NOT NULL
+                );
+            ");
+
+            context.Database.ExecuteSqlCommand(@"
+                CREATE TABLE IF NOT EXISTS Products (
+                    ProductId  INTEGER PRIMARY KEY AUTOINCREMENT,
+                    MaHangHoa  NVARCHAR(20),
+                    TenHangHoa NVARCHAR(100) NOT NULL,
+                    GiaNhap    DECIMAL NOT NULL,
+                    GiaBan     DECIMAL NOT NULL,
+                    CategoryId INTEGER NOT NULL,
+                    FOREIGN KEY (CategoryId) REFERENCES Categories(CategoryId)
+                );
+            ");
+        }
+    }
+}
